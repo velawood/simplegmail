7a5ed8220b45a1c8f7ad18430f76a4d3880d95ac
velawood <alin@velawood.com>
2024-10-11 09:26:09 -0500

    Update gmail.py

--- simplegmail/gmail.py
+++ simplegmail/gmail.py
@@ -14,6 +14,7 @@ from email.mime.image       import MIMEImage
 from email.mime.multipart   import MIMEMultipart
 from email.mime.text        import MIMEText
 import html
+import json
 import math
 import mimetypes
 import os
